7b442a8c25aeaa6451d38b41a6cf6d8e3ea606bf
Leticia Bezerra <leticiabezerrauy@gmail.com>
2025-08-25 11:04:22 -0300

    Primeiro commit do projeto Xcode com MapKit e UIKit

---
+++ PocMapKitBike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocMapKitBike
+//
+//  Created by Leticia Bezerra on 25/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocMapKitBike/PocMapKitBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocMapKitBikeApp.swift
+//  PocMapKitBike
+//
+//  Created by Leticia Bezerra on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct PocMapKitBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
